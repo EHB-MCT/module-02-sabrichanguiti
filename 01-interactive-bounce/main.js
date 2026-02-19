991b88359328a58b2312178610d0cdc5bd5b74a5
sabrichanguiti <changuiti.sabri@gmail.com>
2026-02-19 14:35:10 +0100

    a

--- 01-interactive-bounce/main.js
+++ 01-interactive-bounce/main.js
@@ -1,9 +1,15 @@
 let canvasElement = document.querySelector("#canvas");
 
 //1. Add variables needed for animating the ball
-
+let x = Math.random() * 600
+let y = 300
 //2. Add variables (size, speed, colorVal) for controlling the sketch
+let size = 50
+let speed = 10
+let colorVal = "red"
 
+let speedX = 10
+let directionX = -1
 
 function setup() {
     //Creating a canvas to draw on, using the existing canvas element.
@@ -15,14 +21,21 @@ function draw() {
     background(220);
 
     // 3.1 Animate position
+    x += speedX * directionX
+    y += speedY * directionY
 
     // 3.2 Bounce logic: check the edges
-
+    if(x > width || x < 0){
+        directionX += -1
+    }
     // 3.3 Draw
+    fill(colorVal)
+    circle(x, y, size);
 }
 
 // --- DOM Interaction ---
 
 // 4. Select the input elements
-
+let sizeInput = document.querySelector("#size-input");
 // 5. Add Event Listeners; listen to the "input" event for real-time updates while dragging
+sizeInput.addEventListener("input", function)
